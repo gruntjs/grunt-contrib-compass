d31e0f563bd4e71f9967e5fcd89462f67d08ea63
Sindre Sorhus <sindresorhus@gmail.com>
2013-10-04 18:01:13 +0200

    Fail on compile error and when invalid argument is used
    Fixes #52
    Fixes #54
    Fixes #81

--- tasks/compass.js
+++ tasks/compass.js
@@ -16,10 +16,8 @@ module.exports = function (grunt) {
       cmd: args.shift(),
       args: args
     }, function (err, result, code) {
-      var success = code === 0;
-
       if (code === 127) {
-        return grunt.warn(
+        grunt.warn(
           'You need to have Ruby and Compass installed ' +
           'and in your system PATH for this task to work. ' +
           'More info: https://github.com/gruntjs/grunt-contrib-compass'
@@ -30,11 +28,11 @@ module.exports = function (grunt) {
       // on stderr when it has nothing to compile.
       // https://github.com/chriseppstein/compass/issues/993
       // Don't fail the task in this situation.
-      if (code === 1 && /Nothing to compile/g.test(result.stderr)) {
-        success = true;
+      if (code === 1 && !/Nothing to compile/g.test(result.stderr)) {
+        grunt.warn('↑');
       }
 
-      cb(success);
+      cb();
     });
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
