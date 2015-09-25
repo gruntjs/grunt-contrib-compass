349450a9f544ee7ae8646e8d82e590f1e9138560
Rui Carneiro <rui.arc@gmail.com>
2015-09-25 16:50:14 +0100

    Fix #219: fixing tests

--- test/compass_test.js
+++ test/compass_test.js
@@ -108,7 +108,7 @@ exports.compass = {
 
     // all but the unsupported options are removed.
     test.equal(Object.keys(options).length, 1);
-    test.equal(raw.raw, 'images_path = "/app/images"\nhttp_images_path = "/path/\\"with/\\"quotes"\n');
+    test.equal(raw.raw, 'images_path = \'/app/images\'\nhttp_images_path = \'/path/\\"with/\\"quotes\'\n');
     test.deepEqual(raw.options, ['imagesPath', 'httpImagesPath']);
     test.done();
   },
